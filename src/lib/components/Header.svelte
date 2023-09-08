292a5de40de46ccb4c0e1658ce73478766847a01
Harmoniousmoss <syaefulbahri@protonmail.com>
2023-09-08 09:22:54 +0700

    Header Initial Development

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Hero from './partials/Hero.svelte';
+	import Navbar from './partials/Navbar.svelte';
+</script>
+
+<div class="h-screen bg-gradient-to-b from-cyan-50 to-cyan-100">
+	<Navbar />
+	<Hero />
+</div>
---
+++ src/lib/components/partials/Navbar.svelte
@@ -0,0 +1,63 @@
+<header class="">
+	<div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+		<div class="flex items-center justify-between h-16 lg:h-20">
+			<div class="flex-shrink-0">
+				<a href="/" title="" class="flex">
+					<img
+						class="w-auto h-12"
+						src="https://prospera.or.id/wp-content/uploads/2023/06/Logo-Prospera.png"
+						alt="Logo Prospera"
+					/>
+				</a>
+			</div>
+
+			<button
+				type="button"
+				class="inline-flex p-1 text-black transition-all duration-200 border border-black lg:hidden focus:bg-gray-100 hover:bg-gray-100"
+			>
+				<!-- Menu open: "hidden", Menu closed: "block" -->
+				<svg
+					class="block w-6 h-6"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M4 6h16M4 12h16M4 18h16"
+					/>
+				</svg>
+
+				<!-- Menu open: "block", Menu closed: "hidden" -->
+				<svg
+					class="hidden w-6 h-6"
+					xmlns="http://www.w3.org/2000/svg"
+					fill="none"
+					viewBox="0 0 24 24"
+					stroke="currentColor"
+				>
+					<path
+						stroke-linecap="round"
+						stroke-linejoin="round"
+						stroke-width="2"
+						d="M6 18L18 6M6 6l12 12"
+					/>
+				</svg>
+			</button>
+
+			<div class="hidden ml-auto lg:flex lg:items-center lg:justify-center lg:space-x-10">
+				<a
+					href="https://newsletter.prospera.or.id"
+					title=""
+					class="inline-flex items-center justify-center px-5 py-2.5 text-base font-semibold text-gray-900 border-2 border-cyan-500 hover:bg-cyan-500 hover:text-white transition-all duration-200 focus:bg-cyan-500 focus:text-white rounded-xl"
+					role="button"
+				>
+					Frontpage
+				</a>
+			</div>
+		</div>
+	</div>
+</header>
