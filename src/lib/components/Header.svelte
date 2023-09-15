fb2c11e805c400cc05b2dc776823db69e13af26f
Harmoniousmoss <syaefulbahri@protonmail.com>
2023-09-15 07:42:34 +0700

    Refactor Hero

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -4,7 +4,7 @@
 	export let contentRef;
 </script>
 
-<div class="h-auto bg-gradient-to-t from-cyan-50 to-white lg:h-screen sm:h-auto">
+<div class="h-auto bg-gradient-to-t from-cyan-100 to-white lg:h-screen sm:h-auto">
 	<Navbar />
 	<Hero bind:contentRef />
 </div>
